485fc105812f995b45ed2c5c9a86b573cc74bae3
steven gee <ioexcept@gmail.com>
2022-01-11 13:47:40 -0600

    updated new rosewood

--- rosewood.cmd
+++ rosewood.cmd
@@ -1,10 +1,21 @@
 @echo off
 setlocal
 :PROMPT
-SET /P AREYOUSURE=Restore Last Save (Y/[N])?
+SET /P AREYOUSURE=Create new Rosewood Save File (Y/[N])?
 IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
-rd /s /q "%HOMEPATH%\Zomboid\Saves\Builder\01"
-echo Creating New Archive: Rosewood01
-7z a -tzip %HOMEPATH%\Zomboid\Saves\Rosewood01.zip %HOMEPATH%\Zomboid\Saves\Builder\Rosewood01
+
+	echo Removing old archive file
+	del /f /q %HOMEPATH%\Zomboid\Saves\Rosewood01.zip
+
+	echo Creating New Archive: Rosewood01
+	7z a -tzip %HOMEPATH%\Zomboid\Saves\Rosewood01.zip %HOMEPATH%\Zomboid\Saves\Builder\Rosewood01
+
+:END
+endlocal
+
+
+
+
+
 
 
